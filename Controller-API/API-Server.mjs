ada9b3e0268b68587735669f9a6ec0906b0e1f57
Kaya66 <30756986+Kaya66@users.noreply.github.com>
2024-04-20 23:38:29 -0400

    modified:   Controller-API/API-Server.mjs

--- Controller-API/API-Server.mjs
+++ Controller-API/API-Server.mjs
@@ -15,39 +15,13 @@ let connection = mysql2.createConnection({
 });
 
 //Connect to MySQL server
-connection.connect((err) => {
+connection.connect ( (err) => {
   if (err) return console.error(err.message);
 
   console.log('Connected to MySQL Server.');
 })
 
-
 let sql = 'SHOW COLUMNS FROM Alphabet';
-
-//Create a promise to pull info from mysql server and populate
-let getTable = new Promise(function(myResolve, myReject) {
-  // "Producing Code" (May take some time)
-
-    let table = connection.query(sql, function (err, result) {
-      if (err) throw err;
-      console.log("result: " + result);
-    })
-
-
-    myResolve(table); // when successful
-    myReject();  // when error
-  });
-  
-  // "Consuming Code" (Must wait for a fulfilled Promise)
-  getTable.then(
-    function(value) { return value },
-    function(error) { return "Error" }
-  );
-
-
-
-
-  console.log(table);
 
 
 
