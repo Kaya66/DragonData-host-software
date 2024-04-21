b01c9e93f16e709d13f297ae3dad91a210d36353
Kaya66 <30756986+Kaya66@users.noreply.github.com>
2024-04-20 21:07:28 -0400

    modified:   Controller-API/API-Server.mjs 	new file:   Model-MySQL/setup API User.sql

--- Controller-API/API-Server.mjs
+++ Controller-API/API-Server.mjs
@@ -1,15 +1,16 @@
 // server.mjs
 import { createServer } from 'node:http';
-import mysql from 'mysql';
+import mysql2 from 'mysql2';
+import dotenv from 'dotenv';
+dotenv.config();
 
 //ERROR Auth mode not supported? 
-let connection = mysql.createConnection({
+let connection = mysql2.createConnection({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    database2: process.env.DB_NAME2,
 });
 
 
