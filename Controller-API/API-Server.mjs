a8dd8e6ccc5f2e5be40770d260d5bbc908597cde
Kaya66 <30756986+Kaya66@users.noreply.github.com>
2024-04-21 11:03:42 -0400

    modified:   Controller-API/API-Server.mjs     Script is now capable of connecting to the sql     server and run / return queries.

--- Controller-API/API-Server.mjs
+++ Controller-API/API-Server.mjs
@@ -2,11 +2,10 @@
 import { createServer } from 'node:http';
 import mysql2 from 'mysql2';
 import dotenv from 'dotenv';
-import { table } from 'node:console';
 dotenv.config();
 
 //Setup MySQL Connection
-let connection = mysql2.createConnection({
+const connection = mysql2.createConnection({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
@@ -17,21 +16,22 @@ let connection = mysql2.createConnection({
 //Connect to MySQL server
 connection.connect ( (err) => {
   if (err) return console.error(err.message);
-
   console.log('Connected to MySQL Server.');
 })
 
 let sql = 'SHOW COLUMNS FROM Alphabet';
-
-
-
+connection.query(sql, function (err, results, fields){
+  console.log(results);
+  console.log(fields);
+  }
+);
 
 //Create simple http server on port 3000
 const server = createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
+  
   res.end('Hello World!\n');
 });
-
 
 // run with `node server.mjs`
 server.listen(3000, '127.0.0.1', () => {
