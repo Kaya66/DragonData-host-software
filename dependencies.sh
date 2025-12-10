4c9195753fe77c1433f84801a2f0a4f8c8a8a996
Kaya66 <30756986+Kaya66@users.noreply.github.com>
2025-12-09 23:16:48 -0500

    added sudo to dependencies
    added sudo to dependencies

--- dependencies.sh
+++ dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #Setup script for debian based linux systems
 # NPM must be installed 
-npm install node mysql2 sequelize express
+sudo npm install node mysql2 sequelize express
 #dotenv removed
