ce131375b450e2c9597084183e2966f37a6c4500
Kaya66 <30756986+Kaya66@users.noreply.github.com>
2025-12-11 23:31:04 -0500

    Update SqlRequests.mjs

--- dragondata_next/Database/SqlRequests.mjs
+++ dragondata_next/Database/SqlRequests.mjs
@@ -1,6 +1,6 @@
 import { Sequelize, DataTypes, QueryTypes } from 'sequelize';
 
-const sequelize = new Sequelize('DragonData', process.env.DB_USER, process.env.DB_PASSWORD, {
+const sequelize = new Sequelize('process.env.DB_NAME', process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: 'mysql',
