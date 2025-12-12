2d8230dc912e93ad514db31cc10a5d2ba5f0ee62
Kaya66 <30756986+Kaya66@users.noreply.github.com>
2025-12-11 23:01:57 -0500

    Update setupApiUser.sql

--- dragondata_next/Database/setupApiUser.sql
+++ dragondata_next/Database/setupApiUser.sql
@@ -1,4 +1,4 @@
--- Change user login info. do (NOT) use the default info here in production. 
+-- Change user login info. do (NOT) use the default in production. 
 
 CREATE USER IF NOT EXISTS '@API_USER'@'192.168.%' IDENTIFIED BY '@API_PASSWORD';
 GRANT ALL privileges ON DragonData.* TO '@API_USER'@'192.168.%';
