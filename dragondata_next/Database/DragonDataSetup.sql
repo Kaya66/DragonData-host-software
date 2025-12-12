66c4cc60a0c66fa2e5a4cc151feee69d030a9e6c
Kaya66 <30756986+Kaya66@users.noreply.github.com>
2025-12-11 23:55:03 -0500

    Update DragonDataSetup.sql

--- dragondata_next/Database/DragonDataSetup.sql
+++ dragondata_next/Database/DragonDataSetup.sql
@@ -1,14 +1,16 @@
 DROP DATABASE IF EXISTS DragonData;
 CREATE DATABASE DragonData;
-CREATE TABLE DragonData.Data (
+USE DragonData;
+CREATE TABLE Data (
   url VARCHAR(255) NOT NULL,
-  description VARCHAR(255),
+  description VARCHAR(255)
 );
 
 DROP DATABASE IF EXISTS Auth;
 CREATE DATABASE Auth;
-CREATE TABLE Auth.logins (
+USE Auth;
+CREATE TABLE logins (
   username VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL,
-  APIKey VARCHAR(255) NOT NULL, -- This needs to be auto gen'd with new accounts
-)
+  APIKey VARCHAR(255) NOT NULL -- This needs to be auto gen'd with new accounts
+);
