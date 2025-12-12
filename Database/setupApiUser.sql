b47abc26b2494196cb516904f7aac00f2accfa0d
Kaya66 <Kayadafluff404@gmail.com>
2025-12-11 21:14:56 -0500

    modified:   Database/setupApiUser.sql

--- Database/setupApiUser.sql
+++ Database/setupApiUser.sql
@@ -1,3 +1,5 @@
+-- Change user login info. do (NOT) use the default info here in production. 
+
 CREATE USER IF NOT EXISTS 'ApiUser'@'192.168.%' IDENTIFIED BY '@piUs3rP@ssw0rd';
 GRANT ALL privileges ON DragonData.* TO 'ApiUser'@'192.168.%';
 FLUSH privileges;
