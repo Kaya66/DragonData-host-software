7df4b89241c5a58fce12de9eb4e1845de814438a
Kaya66 <kayadafluff404@gmail.com>
2025-12-15 15:57:21 -0500

    updates to package vesrions.

--- dragondata_next/src/app/Modules/NavBar.js
+++ dragondata_next/src/app/Modules/NavBar.js
@@ -1,9 +1,5 @@
 
 
-
-
-
-
 export default function NavBar() {
     return (
         <section>
--- dragondata_next/src/app/page.js
+++ dragondata_next/src/app/page.js
@@ -7,10 +7,10 @@ import NavBar from "./Modules/NavBar.js";
 export default function Home() {
 
   return (
-        //Flex container for main page
     <div className="flex min-h-screen items-center font-sans">
+          
       <NavBar></NavBar>
-        <main className="flex min-h-screen w-full max-w-3xl flex-col items-center py-32 px-16 bg-white dark:bg-black sm:items-start">
+          <main className="flex min-h-screen w-full max-w-3xl flex-col items-center py-32 px-16 bg-white dark:bg-black sm:items-start">
           <Image
           className="dark:invert"
           src="/next.svg"
